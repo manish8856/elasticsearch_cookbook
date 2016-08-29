593a8720dfd45cff83e444fe08d127aa5d317f35
manish8856 <manish.kumar@crichq.com>
2016-08-29 14:47:53 +0530

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,6 @@
-
+execute "elasticsearch_install" do
+  command 'wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo a  pt-key add -'
+  command "echo "deb http://packages.elastic.co/elasticsearch/#{node['elastic']['version']}/debian stable   main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-#{node['elast  ic']['version']}.list"
+  command 'sudo apt-get update'
+  command 'sudo apt-get -y install elasticsearch'
+end
